da27f74c7344e0186a082af9b5a1bd824bbdb153
Janay Cosme <62524425+jcosme13@users.noreply.github.com>
2021-09-27 21:01:59 -0400

    explored more igraph functions

--- p1.R
+++ p1.R
@@ -12,17 +12,17 @@ library(tidyverse)
 install.packages("reshape2")
 library(reshape2)
 
+## (2) Create graph and plot ##
 # read the data set and assign it to g
 g <- read.table("./dataset.txt", header = FALSE, sep='\t')
 # create a new data frame h based on h
 h <- graph.data.frame(g)
 # plot h
 plot.igraph(h)
-# use omit function to remove any missing data
-omit_h <- na.omit(h)
+
 # plot new simplified graph and delete vertices with degree less than 10
-simplified_h <- delete_vertices(simplify(omit_h), igraph::degree(omit_h)<15 | igraph::degree(omit_h)==0)
-plot.igraph(simplified_h)
+simplified_h <- delete_vertices(simplify(h), igraph::degree(h)==0 | igraph::degree(h)<=10)
+plot.igraph(simplified_h, edge.label = E(simplified_h)$V3)
 
 
 
@@ -32,12 +32,8 @@ plot.igraph(simplified_h)
 # matrix_h <- as.matrix(h, matrix.type = c("adjacency"))
 # edgelist_h <- as_edgelist(h)
 
-#plot.igraph(h, vertex.name = V(h)$V1)
-# use simplify function on h
-# is_simple(h)
-# simplify(h)
-# degree(h)
 
+## (5) ##
 # # find degree centrality
 matrix_g <- graph.edgelist(as.matrix(g[,1:2, drop=FALSE]), directed=FALSE)
 # matrix_g <- simplify(matrix_g)
@@ -66,7 +62,7 @@ matrix_g <- graph.edgelist(as.matrix(g[,1:2, drop=FALSE]), directed=FALSE)
 # find power centrality
 pc <- power_centrality(simplified_h, nodes=V(simplified_h), loops=FALSE)
 
-## (2) additional igraph functions ##
+## (4) additional igraph functions ##
 
 # decompose function: creates a separate graph for each component of the graph
 decompose(simplified_h, mode=c("weak"), max.comps = NA, min.vertices=0)
@@ -85,4 +81,14 @@ is.loop(h)
 
 # knn function: average nearest neighbor degree
 knn(h)
+
+layout_on_sphere(h)
+
+cluster.distribution(h)
+
+coreness(h)
+
+hh<-permute(simplified_h, sample(vcount(simplified_h)))
+plot.igraph(hh)
+
 
