5c33c17c5e48618b3ab14b17e91cf3ff6943157f
Janay Cosme <62524425+jcosme13@users.noreply.github.com>
2021-10-01 17:55:18 -0400

    simplify graph, clean up code

--- p1.R
+++ p1.R
@@ -1,18 +1,11 @@
-# installing all the packages
+# installing packages
 install.packages("igraph")
 library(igraph)
-install.packages("plyr")
-library(plyr)
 install.packages("sna")
 library(sna)
-install.packages("CINNA")
-library(CINNA)
-install.packages("tidyverse")
-library(tidyverse)
-install.packages("reshape2")
-library(reshape2)
 
 ## (2) Create graph and plot ##
+
 # read the data set and assign it to g
 g <- read.table("./dataset.txt", header = FALSE, sep='\t')
 # create a new data frame h based on h
@@ -20,11 +13,9 @@ h <- graph.data.frame(g)
 # plot h
 plot.igraph(h)
 
-# plot new simplified graph and delete vertices with degree less than 10
-simplified_h <- delete_vertices(simplify(h), igraph::degree(h)==0 | igraph::degree(h)<=10)
+# simplify h by plotting from the range 1:100
+simplified_h <- subgraph.edges(h, 1:100)
 plot.igraph(simplified_h, edge.label = E(simplified_h)$V3)
-
-
 
 # convert g to matrix
 # matrix <- as.matrix(g[,1:2, drop=FALSE])
@@ -32,63 +23,60 @@ plot.igraph(simplified_h, edge.label = E(simplified_h)$V3)
 # matrix_h <- as.matrix(h, matrix.type = c("adjacency"))
 # edgelist_h <- as_edgelist(h)
 
-
-## (5) ##
-# # find degree centrality
-matrix_g <- graph.edgelist(as.matrix(g[,1:2, drop=FALSE]), directed=FALSE)
-# matrix_g <- simplify(matrix_g)
-# 
-# 
-# # closeness centrality
-# igraph::centr_clo(h) # centralization: 0.1661062, theoretical max: 26473
-# 
-# # betweenness centrality
-# igraph::centr_betw(h) # centralization: 0.1536933, theoretical max: 1.85542e+13
-# #h_net
-# #V(h)$degree<-degree(h)
-# #plot(h,vertex.label.cex = .6, vertex.label.color = "black", vertex.size = V(h)$degree, vertex.label.cex = .2)
-# 
-# # determine longest paths 
-# farthest_vertices(h, directed=FALSE) # $vertices
-# # + 2/26475 vertices, named, from 9705623:
-# # [1] 23601 3010, distance: 17
-# 
-# # get diameter
-# diameter(h, directed=FALSE) # 17
-# 
-# # find largest cliques
-# h.lgcliques=igraph::clique_num(matrix_g) # 16
-
-# find power centrality
-pc <- power_centrality(simplified_h, nodes=V(simplified_h), loops=FALSE)
-
 ## (4) additional igraph functions ##
 
 # decompose function: creates a separate graph for each component of the graph
 decompose(simplified_h, mode=c("weak"), max.comps = NA, min.vertices=0)
 
 # mean distance function: calculates the average path length in the graph
-mean_distance(h)
+mean_distance(simplified_h)
 
 # radius function
-radius(h)
+radius(simplified_h)
 
 # hub score function: Kleinberg's hub centrality scores
-hub_score(h)$vector
+hub_score(simplified_h)$vector
 
 # is loop function: determines if graph contains loops
-is.loop(h)
+is.loop(simplified_h)
 
 # knn function: average nearest neighbor degree
-knn(h)
+knn(simplified_h)
 
-layout_on_sphere(h)
+# layout on sphere function: places the vertices (approximately) uniformly on 
+# the surface of a sphere
+layout_on_sphere(simplified_h)
 
-cluster.distribution(h)
+# cluster distribution function: calculate the maximal (weakly or strongly) 
+# connected components of a graph
+cluster.distribution(simplified_h)
 
-coreness(h)
+# coreness function: the k-core of graph is a maximal subgraph in which each 
+# vertex has at least degree k
+coreness(simplified_h)
 
+# permute function: create a new graph by permuting vertex ids
 hh<-permute(simplified_h, sample(vcount(simplified_h)))
 plot.igraph(hh)
 
+## (5) ##
 
+# closeness centrality
+igraph::centr_clo(simplified_h) 
+
+# betweenness centrality
+igraph::centr_betw(simplified_h)
+
+# determine longest paths
+farthest_vertices(simplified_h, directed=FALSE)
+
+# find largest cliques
+clique_num(simplified_h)
+
+#ego
+ego_calc<-ego(simplified_h)
+rand <- sample(ego_calc, 10)
+print(rand)
+
+# find power centrality
+pc <- power_centrality(simplified_h, nodes=V(simplified_h), loops=FALSE, exponent=0.9)
